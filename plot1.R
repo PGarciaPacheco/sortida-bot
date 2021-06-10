78524b2ce1197077f31559a137797660f265d265
Paula Garcia <paula.garcia20@estudiant.upf.edu>
2021-06-10 12:24:22 +0200

    changes in plot1

--- plot1.R
+++ plot1.R
@@ -2,4 +2,4 @@ dat <- read.csv("plantesexcursio.csv", sep=";", stringsAsFactors=TRUE)
 dist <- dat[ ,"DISTÀNCIA.AL.CAMÍ"]
 distancia <- dist[!duplicated(dist)]
 riquesa <- (c(13,12,8,8,8,7))
-plot.default(distancia,riquesa, main="Plot 1")
+plot.default(distancia,riquesa, main="Variació riquesa segons distància al camí", frame.plot=FALSE, pch=16, col=4, type="b")
